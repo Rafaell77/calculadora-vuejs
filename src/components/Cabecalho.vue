76b6b8ce0958a8e0c804ede13a2842a2a1c5a6b3
Rafael <Rafael@git.com>
2024-05-19 13:25:45 -0300

    att o app

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,13 @@
+
+<script setup>
+    const propos = defineProps(['tarefasPendentes'])
+</script>
+
+<template>
+    <header class="p-5 mb-4 bg-light rounded-3">
+        <h1>Minhas tarefas</h1>
+        <p>
+            Você possui {{ propos.tarefasPendentes }} tarefas pendentes
+        </p>
+    </header>
+</template>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,27 @@
+
+<script setup>
+    const propos = defineProps(['cadastraTarefa', 'tarefaTemp', 'trocaFiltro', 'editaTarefaTemp'])
+</script>
+
+<template>
+<form @submit.prevent ="propos.cadastraTarefa">
+    <div class="row">
+    <div class="col">
+        <input :value="propos.tarefaTemp" @change=" propos.editaTarefaTemp " required 
+        type="text" placeholder="Digite aqui a descrição da tarefa" class="form-control">
+    </div>
+    <div class="col-md-2">
+        <button type="submit" class="btn btn-primary">Cadastrar</button>
+    </div>
+    <div class="col-md-2">
+    <div class="col-md-2">
+        <select @change="propos.trocaFiltro" class="form-control">
+            <option value="todas">Todas tarefas</option>
+            <option value="pendentes">Pendentes</option>
+            <option value="finalizadas">Finalizadas</option>
+        </select>
+    </div>
+    </div>
+    </div>
+</form>
+</template>
